daf08fa8d13c740468b7b72d83186c09ba074d2d
latarho <latarho@gmail.com>
2020-04-18 18:20:03 +0500

    Added test add contact

---
+++ contact.py
@@ -0,0 +1,12 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, title, company, address, mobile, email, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.notes = notes
