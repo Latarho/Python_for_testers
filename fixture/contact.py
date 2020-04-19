e45c21427c65ced1bbdc9f3ada28de4ed20a4f91
latarho <latarho@gmail.com>
2020-04-19 11:16:38 +0500

    Added test modify first contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -47,6 +47,45 @@ class ContactHelper:
         driver.find_element_by_xpath("//*[@id='content']/form[2]/div[2]/input").click()
         driver.switch_to_alert().accept()
 
+    def modify_first_contact(self, contact):
+        driver = self.app.driver
+        # select first contact
+        driver.find_element_by_name("selected[]").click()
+        # init contact modify
+        driver.find_element_by_xpath("//*[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        # fill contact form
+        driver.find_element_by_name("firstname").click()
+        driver.find_element_by_name("firstname").clear()
+        driver.find_element_by_name("firstname").send_keys(contact.first_name)
+        driver.find_element_by_name("middlename").click()
+        driver.find_element_by_name("middlename").clear()
+        driver.find_element_by_name("middlename").send_keys(contact.middle_name)
+        driver.find_element_by_name("lastname").click()
+        driver.find_element_by_name("lastname").clear()
+        driver.find_element_by_name("lastname").send_keys(contact.last_name)
+        driver.find_element_by_name("title").click()
+        driver.find_element_by_name("title").clear()
+        driver.find_element_by_name("title").send_keys(contact.title)
+        driver.find_element_by_name("company").click()
+        driver.find_element_by_name("company").clear()
+        driver.find_element_by_name("company").send_keys(contact.company)
+        driver.find_element_by_name("address").click()
+        driver.find_element_by_name("address").clear()
+        driver.find_element_by_name("address").send_keys(contact.address)
+        driver.find_element_by_name("mobile").click()
+        driver.find_element_by_name("mobile").clear()
+        driver.find_element_by_name("mobile").send_keys(contact.mobile)
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys(contact.email)
+        driver.find_element_by_name("notes").click()
+        driver.find_element_by_name("notes").clear()
+        driver.find_element_by_name("notes").send_keys(contact.notes)
+        # submit modify
+        driver.find_element_by_xpath("//*[@id='content']/form[1]/input[22]").click()
+        # return to contact page from update
+        driver.find_element_by_xpath("//*[@id='content']/div/i/a").click()
+
     def return_to_contact_page(self):
         driver = self.app.driver
         driver.find_element_by_xpath("//*[@id='content']/div/i/a[2]").click()
---
+++ test/test_modify_contact.py
@@ -0,0 +1,8 @@
+from model.contact import Contact
+
+def test_modify_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(first_name="Chebi", middle_name="Chebikovich", last_name="Genarek",
+                                            title="Lil", company="Chebi GANG", address="Surikova street",
+                                            mobile="8-800-555-555-555", email="orange@orange.com", notes="Little Gangster"))
+    app.session.logout()
