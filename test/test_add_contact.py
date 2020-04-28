df41159b198992a34b313e7ed23f5c23c3260289
latarho <latarho@gmail.com>
2020-04-28 20:15:26 +0500

    Complete part 6

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -18,8 +18,7 @@ testdata = [Contact(first_name="", middle_name="", last_name="", title="", compa
 def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
     app.contact.create_contact(contact)
-    new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == app.contact.count()
-
+    new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,15 +5,11 @@ def test_modify_some_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="test", footer="test"))
     old_groups = app.group.get_group_list()
-    # Определение случайного индекса группы
-    # Функция randrange генерирует значение от 0 до указанного (в данном случае len(old_groups)
     index = randrange(len(old_groups))
     group = Group(name="Modify_group_1")
     group.id = old_groups[index].id
     app.group.modify_group_by_index(index, group)
-
-    new_groups = app.group.get_group_list()
     assert len(old_groups) == app.group.count()
-
+    new_groups = app.group.get_group_list()
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
